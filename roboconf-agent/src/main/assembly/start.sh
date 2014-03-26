1392006d07a658ae34f14182978c8561bbebd135
vzurczak <vincent.zurczak@linagora.com>
2014-03-26 15:51:11 +0100

    Add logging in the DM (packaged as a web app') + Rename the logger properties file

--- roboconf-agent/src/main/assembly/start.sh
+++ roboconf-agent/src/main/assembly/start.sh
@@ -1,3 +1,3 @@
 # Can be started locally with 4 params:
 # applicationName, rootInstanceName (= machine name), ipMessagingServer, ipAddress
-java -Djava.util.logging.config.file=./loggers.properties -cp "lib/*" net.roboconf.agent.internal.Main $*
+java -Djava.util.logging.config.file=./logging.properties -cp "lib/*" net.roboconf.agent.internal.Main $*
