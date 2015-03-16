2b904f395b33c0823b85ae7dd6bba562b07a565e
Vincent Zurczak <vincent.zurczak@gmail.com>
2015-03-16 17:24:08 +0100

    #203 Validate the presence of target handlers on deployment

--- core/roboconf-dm/src/main/java/net/roboconf/dm/internal/environment/target/TargetHelpers.java
+++ core/roboconf-dm/src/main/java/net/roboconf/dm/internal/environment/target/TargetHelpers.java
@@ -28,14 +28,20 @@ package net.roboconf.dm.internal.environment.target;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import net.roboconf.core.Constants;
 import net.roboconf.core.model.beans.Instance;
 import net.roboconf.core.model.helpers.InstanceHelpers;
 import net.roboconf.core.utils.ResourceUtils;
 import net.roboconf.core.utils.Utils;
+import net.roboconf.dm.management.ITargetResolver;
+import net.roboconf.dm.management.ManagedApplication;
+import net.roboconf.target.api.TargetException;
+import net.roboconf.target.api.TargetHandler;
 
 /**
  * Utilities related to deployment targets.
@@ -64,12 +70,35 @@ public final class TargetHelpers {
 		File f = ResourceUtils.findInstanceResourcesDirectory( applicationFilesDirectory, realRootInstance );
 		f = new File( f, Constants.TARGET_PROPERTIES_FILE_NAME );
 
-		Map<String, String> result = new HashMap<String,String>();
+		Map<String,String> result = new HashMap<String,String>();
 		Properties p = Utils.readPropertiesFile( f );
 		for( Map.Entry<Object,Object> entry : p.entrySet()) {
 			result.put( entry.getKey().toString(), entry.getValue().toString());
 		}
 
 		return result;
+	}
+
+
+	/**
+	 * Verifies that all the target handlers an application needs are installed.
+	 * @param targetResolver the DM's target resolver
+	 * @param ma a managed application
+	 * @param handlers a non-null list of handlers
+	 */
+	public static void verifyTargets( ITargetResolver targetResolver, ManagedApplication ma, List<TargetHandler> handlers ) {
+
+		Logger logger = Logger.getLogger( TargetHelpers.class.getName());
+		for( Instance inst : InstanceHelpers.getAllInstances( ma.getApplication())) {
+			if( ! InstanceHelpers.isTarget( inst ))
+				continue;
+
+			try {
+				targetResolver.findTargetHandler( handlers, ma, inst );
+
+			} catch( TargetException e ) {
+				logger.warning( e.getMessage());
+			}
+		}
 	}
 }
