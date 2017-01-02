460fdfe613eab7879359a0c9ef44dcf0e3ba266c
Vincent Zurczak <vincent.zurczak@gmail.com>
2017-01-02 12:54:46 +0100

    #739 Exception in integration tests

--- miscellaneous/roboconf-integration-tests-dm/src/test/java/net/roboconf/integration/tests/dm/KarafCommandsRequirementsTest.java
+++ miscellaneous/roboconf-integration-tests-dm/src/test/java/net/roboconf/integration/tests/dm/KarafCommandsRequirementsTest.java
@@ -49,6 +49,7 @@ import net.roboconf.core.Constants;
 import net.roboconf.core.internal.tests.TestUtils;
 import net.roboconf.core.utils.ManifestUtils;
 import net.roboconf.dm.management.Manager;
+import net.roboconf.dm.scheduler.IScheduler;
 import net.roboconf.integration.tests.commons.AbstractIntegrationTest;
 import net.roboconf.integration.tests.commons.ItConfigurationBean;
 import net.roboconf.integration.tests.commons.internal.ItUtils;
@@ -65,6 +66,9 @@ public class KarafCommandsRequirementsTest extends DmTest {
 
 	@Inject
 	protected Manager manager;
+
+	@Inject
+	protected IScheduler scheduler;
 
 
 	@ProbeBuilder
